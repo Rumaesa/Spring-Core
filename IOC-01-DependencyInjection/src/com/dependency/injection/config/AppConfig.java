36509effeaff8629ca7c94a87c2a474dd15ba96c
Ansari Rumaesa <rumaesaansari@gmail.com>
2024-05-15 11:46:37 +0530

    Field Injection
    Code Commit for Dependency Injection using Field Injection.
    
    Injected LocalDateTime object using field injection.
    
    output:

---
+++ IOC-01-DependencyInjection/src/com/dependency/injection/config/AppConfig.java
@@ -0,0 +1,24 @@
+package com.dependency.injection.config;
+
+import java.time.LocalDateTime;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.dependency.injection.sbeans")
+public class AppConfig {
+	
+	public AppConfig() {
+		System.out.println("AppConfig.AppConfig() : 0-Parameter Constructor");
+	}
+
+	@Bean(name="dt")
+	public LocalDateTime createLdt() {
+		System.out.println("AppConfig.createLdt()");
+		return LocalDateTime.now();
+	}
+	
+	
+}
